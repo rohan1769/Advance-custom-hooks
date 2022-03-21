70a423e1de4960486f6c6ad69bacf407fede1dcf
Rohan Singh <1808034@kiit.ac.in>
2022-03-21 12:58:16 +0530

    custom hook created

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-const useHttp = () => {
+import { useState } from "react";
+const useHttp = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  
 
   const sendRequest = async (taskText) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        "https://react-http-3fe05-default-rtdb.firebaseio.com/tasks.json",
-      );
+      const response = await fetch(requestConig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
 
       if (!response.ok) {
         throw new Error("Request failed!");
       }
 
       const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+      applyData(data);
 
       setTasks(loadedTasks);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
+  };
+  return {
+    isLoading,
+    error,
+    sendRequest,
   };
 };
 
