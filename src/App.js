5704086b54a1dbef526541a4cc4ff537888ef51c
Rohan Singh <1808034@kiit.ac.in>
2022-03-22 16:08:12 +0530

    final without callBack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -6,20 +6,24 @@ import useHTTP from "./hooks/use-http";
 import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTasks = useCallback((tasksObj) => {
-    const loadedTasks = [];
 
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  }, []);
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
   useEffect(() => {
-    fetchTasks({
-      url: "https://react-http-3fe05-default-rtdb.firebaseio.com/tasks.json",
-    });
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: "https://react-http-3fe05-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
